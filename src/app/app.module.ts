9086a9cf52ebc200c5fec89193152502459475ee
Wesley <wesmm555@icloud.com>
2022-07-08 22:13:22 -0300

    Segundo Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Teste1Component } from './teste1/teste1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Teste1Component
+
   ],
   imports: [
     BrowserModule
